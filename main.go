5f476c9c528fcba6f16d5b1544406974222d4911
Somebro <someBroYouKnow@users.noreply.github.com>
2025-10-08 05:34:10 +0530

    docker and redis initialised

--- main.go
+++ main.go
@@ -14,4 +14,13 @@ func main() {
 		fmt.Println("Error in creating form, try again next time ")
 	}
 	fmt.Printf("Input URL= %v", formData.InputUrl)
+
+	// Generate some unique non-collisionary ID for the URL
+
+	// Save the ID in redis
+
+	// Setup nginx, or a go lang server that does the hopping from one URL to the other
+
+	// Check if analytics can be placed here
+
 }
